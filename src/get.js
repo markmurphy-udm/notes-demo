8aa99243205f890e9841056b413072dcf78c978b
Mark Murphy <mark.murphy@umbrelladm.com>
2022-01-05 12:43:46 -0800

    faulty code here

--- src/get.js
+++ src/get.js
@@ -3,17 +3,21 @@ import dynamoDb from "./util/dynamodb";
 
 export const main = handler(async (event) => {
   const params = {
-    TableName: process.env.TABLE_NAME,
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
     Key: {
       userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      noteId: event.pathParameters.id,
-    },
+      // noteId: event.pathParameters.id
+    }
   };
 
   const result = await dynamoDb.get(params);
-  if (!result.Item) {
+  if ( ! result.Item) {
     throw new Error("Item not found.");
   }
 
+  // Return the retrieved item
   return result.Item;
 });
